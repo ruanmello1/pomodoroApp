d1ce9166a8848e3c79a2bdb7271ecb4ab5432af2
Ruan Mello <ruan.mello0123@gmail.com>
2022-10-27 11:43:06 -0300

    Adicionando arquivos

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { PomodoroTimer } from './components/pomodoro-timer'
+
+function App(): JSX.Element {
+  return (
+    <div className="container">
+      <PomodoroTimer pomodoroTime={1500} shortRestTime={300} longRestTime={900} cycles={4}/>
+    </div>
+  );
+}
+
+export default App;
