98e8eb4efabd40b01d650c78f0daa395a9fc1440
Ruan Mello <ruan.mello0123@gmail.com>
2022-10-27 12:05:14 -0300

    Adicionando funcionalida aos botões

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useInterval } from "../hooks/use-interval";
 import { Button } from "./button";
 import { Timer } from "./timer";
@@ -12,19 +12,30 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.pomodoroTime)
+  const [timeCounting, setTimeCounting] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
+
+  useEffect(() => {
+    if(working) document.body.classList.add('working');
+  }, [working])
 
   useInterval(() => {
     setMainTime(mainTime - 1);
-  }, 1000)
+  }, timeCounting ? 1000 : null);
+
+  const configureWork = () => {
+    setTimeCounting(true);
+    setWorking(true);
+  }
 
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
+        <Button text="Work" onClick={() => configureWork()}></Button>
         <Button text="teste" onClick={() => console.log(1)}></Button>
-        <Button text="teste" onClick={() => console.log(1)}></Button>
-        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text={timeCounting ? 'Pause' : 'Play'} onClick={() => setTimeCounting(!timeCounting)}></Button>
       </div>
 
       <div className="details">
